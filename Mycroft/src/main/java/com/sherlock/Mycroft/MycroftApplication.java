63c254aad01961c47e381cb06cadc725accb7dcc
Saleh Shehata <Sal@localhost.localdomain>
2018-05-30 19:01:12 -0400

    Init Backend Server 'Mycroft'.

---
+++ Mycroft/src/main/java/com/sherlock/Mycroft/MycroftApplication.java
@@ -0,0 +1,12 @@
+package com.sherlock.Mycroft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycroftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycroftApplication.class, args);
+	}
+}
